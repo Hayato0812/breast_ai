f90cc7b93b91fa8a934878ce714a51d7371c8305
Hayato0812 <hayato08120904@gmail.com>
2019-11-23 20:25:53 +0900

    とりあえずapi連携

---
+++ config.py
@@ -0,0 +1,4 @@
+CONSUMER_KEY = 'QYJns9MZU9MlJ3do2yQLjCLqm'                           # Consumer Key
+CONSUMER_SECRET = 'N5l98L6EpjIU1PudtkZh5t8aSpi32ROPcjnM874QYDSjFXcYCh'       # Consumer Secret
+ACCESS_TOKEN = '1197837220918390785-XHatX4HZMuzRzt3IqEveCCHl4cff0X' # Access Token
+ACCESS_TOKEN_SECRET = 'gJcXvHIUzDKkON3XBqhJDUz27ach38rRUxTwAGvG2XRvR'         # Accesss Token Secert
